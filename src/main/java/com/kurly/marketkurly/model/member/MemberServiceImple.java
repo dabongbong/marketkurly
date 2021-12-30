23ed7713b38d760f4f8fc870d26e422785be2798
ojm1484 <86396865+ojm1484@users.noreply.github.com>
2021-12-30 17:52:49 +0900

    jm211230

---
+++ src/main/java/com/kurly/marketkurly/model/member/MemberServiceImple.java
@@ -0,0 +1,31 @@
+package com.kurly.marketkurly.model.member;
+
+import java.util.List;
+
+import com.kurly.marketkurly.domain.Member;
+
+public class MemberServiceImple implements MemberService{
+
+	@Override
+	public List selectAll() {
+		return null;
+	}
+
+	@Override
+	public Member select(int member_id) {
+		return null;
+	}
+
+	@Override
+	public void insert(Member member) {
+	}
+
+	@Override
+	public void update(Member member) {
+	}
+
+	@Override
+	public void delete(int member_id) {
+	}
+
+}
---
+++ src/main/java/com/kurly/marketkurly/model/member/MybatisMemberDAO.java
@@ -0,0 +1,51 @@
+package com.kurly.marketkurly.model.member;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kurly.marketkurly.domain.Member;
+import com.kurly.marketkurly.exception.MemberException;
+
+@Repository
+public class MybatisMemberDAO implements MemberDAO{
+	@Autowired
+	private SqlSessionTemplate sessionTemplate;
+
+	@Override
+	public List selectAll() {
+		return sessionTemplate.selectList("Member.selectAll");
+	}
+
+	@Override
+	public Member select(int member_id) {
+		return sessionTemplate.selectOne("Member.select",member_id);
+	}
+
+	@Override
+	public void insert(Member member)throws MemberException{
+		int result=sessionTemplate.insert("Member.insert", member);
+		if(result==0) {
+			throw new MemberException("회원 등록 실패");
+		}
+	}
+
+	@Override
+	public void update(Member member) {
+		int result=sessionTemplate.update("Member.update",member);
+		if(result==0) {
+			throw new MemberException("회원 수정 실패");
+		}
+	}
+
+	@Override
+	public void delete(int member_id) throws MemberException{
+		int result=sessionTemplate.delete("Member.delete",member_id);
+		if(result==0) {
+			throw new MemberException("회원삭제 실패");
+		}
+	}
+
+}
