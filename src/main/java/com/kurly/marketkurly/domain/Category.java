a5523c90e62f65ba2cd05fba3d5a07b7a7846cc1
sunny-im <hellossun.dahae@gmail.com>
2021-12-30 21:36:34 +0900

    sunny 1230

--- src/main/java/com/kurly/marketkurly/domain/Category.java
+++ src/main/java/com/kurly/marketkurly/domain/Category.java
@@ -1,6 +1,5 @@
 package com.kurly.marketkurly.domain;
 
-<<<<<<< Updated upstream
 import lombok.Data;
 
 @Data
@@ -8,8 +7,5 @@ public class Category {
 	private int category_id;
 	private String category_name;
 	private String category_logo;
-=======
-public class Category {
 
->>>>>>> Stashed changes
 }
