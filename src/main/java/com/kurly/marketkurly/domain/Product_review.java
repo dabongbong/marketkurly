f362c0b50fa2e57acf88d799519566e6ff49f0b3
dabongbong <killers61@icloud.com>
2021-12-30 17:22:47 +0900

    dabong/1230

--- src/main/java/com/kurly/marketkurly/domain/Product_review.java
+++ src/main/java/com/kurly/marketkurly/domain/Product_review.java
@@ -3,7 +3,7 @@ package com.kurly.marketkurly.domain;
 import lombok.Data;
 
 @Data
-public class Product_revew {
+public class Product_review {
 	private int review_no;
 	private Product product;
 	private String title;
