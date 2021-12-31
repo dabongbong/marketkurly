a8877dce891f96d1628dd0f0bf5a1236b650954e
dabongbong <killers61@icloud.com>
2021-12-31 15:04:08 +0900

    dabong/12.31

--- src/main/java/com/kurly/marketkurly/model/product/ProductServiceImpl.java
+++ src/main/java/com/kurly/marketkurly/model/product/ProductServiceImpl.java
@@ -2,31 +2,28 @@ package com.kurly.marketkurly.model.product;
 
 import java.util.List;
 
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import com.kurly.marketkurly.domain.Product;
+import com.kurly.marketkurly.domain.Product_detail;
+import com.kurly.marketkurly.domain.Product_hashtag;
 
-@Repository
-public class MybatisProductDAO implements ProductDAO{
-	
+public class ProductServiceImpl implements ProductService{
 	@Autowired
-	private SqlSessionTemplate sessionTemplate;
+	private ProductDAO productDAO;
 
 	@Override
 	public List selectAll() {
-		return sessionTemplate.selectList("Product.selectAll");
+		return productDAO.selectAll();
 	}
 
 	@Override
 	public Product select(int product_id) {
-		return sessionTemplate.selectOne("Product.select", product_id);
+		return productDAO.select(product_id);
 	}
 
 	@Override
-	public void insert(Product product) {
-		// TODO Auto-generated method stub
+	public void insert(Product product, List<Product_detail> prouct_detail, List<Product_hashtag> product_hashtag) {
 		
 	}
 
