2f014f668e15e8d772e84bc753748f102e647930
ojm1484 <86396865+ojm1484@users.noreply.github.com>
2021-12-31 14:04:59 +0900

    1231

--- src/main/java/com/kurly/marketkurly/controller/admin/MemberController.java
+++ src/main/java/com/kurly/marketkurly/controller/admin/MemberController.java
@@ -1,19 +1,6 @@
 package com.kurly.marketkurly.controller.admin;
 
-import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.kurly.marketkurly.model.member.MemberService;
-import com.kurly.marketkurly.util.Pager;
-
-@Controller
 public class MemberController {
 	
 }
