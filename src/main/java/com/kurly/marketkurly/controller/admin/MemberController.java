0487ce167201e4b88bbbdc8671593cb8339d60f0
ojm1484 <86396865+ojm1484@users.noreply.github.com>
2021-12-31 14:02:36 +0900

    jm1231

--- src/main/java/com/kurly/marketkurly/controller/admin/MemberController.java
+++ src/main/java/com/kurly/marketkurly/controller/admin/MemberController.java
@@ -15,19 +15,5 @@ import com.kurly.marketkurly.util.Pager;
 
 @Controller
 public class MemberController {
-	@Autowired
-	private MemberService memberService;
-	@Autowired
-	private Pager pager;
 	
-	
-	@GetMapping("/member/list")
-	public String getList(HttpServletRequest request,Model model) {
-		List memberList=memberService.selectAll();
-		pager.init(memberList, request);
-		
-		model.addAttribute("memberList",memberList);
-		model.addAttribute("pager",pager);
-		return "admin/member/list";
-	}
 }
