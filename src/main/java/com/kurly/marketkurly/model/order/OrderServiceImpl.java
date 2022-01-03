9d2fe17d308cbdcbff33475e64b80b940e084f78
SungHyun Kim <lms10234@naver.com>
2022-01-03 14:31:40 +0900

    김성현 22.01.03

--- src/main/java/com/kurly/marketkurly/model/order/OrderServiceImpl.java
+++ src/main/java/com/kurly/marketkurly/model/order/OrderServiceImpl.java
@@ -11,9 +11,6 @@ public class OrderServiceImpl implements OrderService {
 	@Autowired
 	private OrderSummaryDAO orderSummaryDAO;
 	
-	@Autowired
-	private OrderDetailDAO orderDetailDAO;
-	
 	@Override
 	public List selectAll() {
 		return orderSummaryDAO.selectAll();
