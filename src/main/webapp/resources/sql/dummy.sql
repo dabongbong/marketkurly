848a906ab06e1040f6e323eda8069054cd4007b5
SungHyun Kim <lms10234@naver.com>
2022-01-04 17:49:26 +0900

    ksh 0104

--- src/main/webapp/resources/sql/dummy.sql
+++ src/main/webapp/resources/sql/dummy.sql
@@ -5,4 +5,5 @@ values(1,'1234','홍길동','ooo11@naver.com','01012341234','경기도 의왕시
 insert into category (category_name, category_logo) values('채소', '식이섬유 가득한 채소나으리');
 insert into subcategory (category_id, subcategory_name) values (1, '내가 제일 싫어하는 빨간색 음식');
 insert into product (subcategory_id, title, subtitle, price, detail, sale) values(1, '토마토', '방울',15000,'이게 맛있냐?',0);
+insert into order_summary (member_id, paymethod_id, price) values (1,3,15000);
 insert into order_detail (order_summary_id,product_id,price) values(3,1,15000);
