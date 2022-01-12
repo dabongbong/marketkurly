11f7526e2053832e3fc2f1ffce6683c4ebcb5a12
dabongbong <killers61@icloud.com>
2022-01-13 01:14:01 +0900

    dabong/1.13

--- src/main/java/com/kurly/marketkurly/model/product/ProductService.java
+++ src/main/java/com/kurly/marketkurly/model/product/ProductService.java
@@ -3,11 +3,10 @@ package com.kurly.marketkurly.model.product;
 import java.util.List;
 
 import com.kurly.marketkurly.domain.Product;
-import com.kurly.marketkurly.domain.ProductDetail;
-import com.kurly.marketkurly.domain.ProductHashtag;
 
 public interface ProductService {
 	public List selectAll();
+	public List selectByCategory(int category_id);
 	public Product select(int product_id);
 	public void insert(Product product);
 	public void update(Product product);
