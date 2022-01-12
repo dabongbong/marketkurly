ebaab689fd7dc46dec524c7edaf2f39724a50e12
dabongbong <killers61@icloud.com>
2022-01-13 05:11:05 +0900

    dabong/main

--- src/main/java/com/kurly/marketkurly/controller/admin/MainController.java
+++ src/main/java/com/kurly/marketkurly/controller/admin/MainController.java
@@ -2,22 +2,35 @@ package com.kurly.marketkurly.controller.admin;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kurly.marketkurly.model.member.MemberService;
+import com.kurly.marketkurly.model.order.OrderService;
+
 
 @Controller
 public class MainController {
+	@Autowired
+	private OrderService orderService;
+	@Autowired
+	private MemberService memberService;
 	
-		@RequestMapping(value="/main", method=RequestMethod.GET)
-		public ModelAndView getMain(HttpServletRequest request) {
-			
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("admin/index");
-			
-			return mav;
+	
+	
+	@RequestMapping(value="/main", method=RequestMethod.GET)
+	public ModelAndView getMain(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		int newOrder = orderService.checkNewOrder();
+		int regToday = memberService.regToday();
+		mav.addObject("newOrder", newOrder);
+		mav.addObject("regToday", regToday);
+		mav.setViewName("admin/index");
+		
+		return mav;
 		}	
 	}		
 
