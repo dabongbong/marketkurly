2af5937582eee3f679af174471afbc628f003126
SungHyun Kim <lms10234@naver.com>
2022-01-13 05:35:01 +0900

    ksh 0113

--- src/main/webapp/resources/sql/dummy.sql
+++ src/main/webapp/resources/sql/dummy.sql
@@ -1,4 +1,6 @@
 insert into admin(user_id, pass, name) values('admin','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','관리자');
+insert into member(user_id, member_pass,member_name,email,phone,addr,gender,birth)
+value('asd1234','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','김바보','babo@gmai.com','PstRWPYO6Kfep4VLaWtcFzPy473D4Cjl2Pg1/T0mGec=','서울','남','19190202');
 
 insert into paymethod (method) values('카카오결제');
 insert into paymethod (method) values('신용카드');
